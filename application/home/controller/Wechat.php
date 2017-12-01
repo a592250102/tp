9361e3a53858e3947fc86e7e8ec065733568fb24
hjz <592250102@qq.com>
2017-12-01 11:32:32 +0800

    1

--- application/home/controller/Wechat.php
+++ application/home/controller/Wechat.php
@@ -9,7 +9,7 @@ class Wechat extends Controller {
         //1.1 引导关注者打开页面:https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
         $appid = 'wx1677dd8c02f442a0';
         $callback_url = url('home/wechat/callback','',true,true);
-        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$callback_url}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$callback_url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
         //跳转
         $this->redirect($url);
     }
